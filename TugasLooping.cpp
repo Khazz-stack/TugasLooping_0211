e2e2301f3722f80ecc5550ff14e8e9116345a381
Khazz <thelenaid@gmail.com>
2026-03-31 16:03:54 +0700

    komentar setiap function

--- TugasLooping.cpp
+++ TugasLooping.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-
+// variabel global
 int angka;
 int pilihan;
-
+// cek bilangan prima
 bool cekPrima() {
     if (angka < 2) return false;
 
@@ -14,7 +14,7 @@ bool cekPrima() {
     }
     return true;
 }
-
+// cek bilangan fibonacci
 bool cekFibonacci(){
     int a = 0, b = 1;
 
@@ -26,26 +26,26 @@ bool cekFibonacci(){
 
     return (b == angka || angka == 0);
 }
-
+// prosedur input angka
 void inputAngka(){
     cout << "Masukan angka: ";
     cin >> angka;
 }
-
+// prosedur tampil hasil prima
 void tampilHasilPrima(){
     if (cekPrima())
         cout << angka << " adalah bilangan prima." << endl;
     else
         cout << angka << " bukan bilangan prima." << endl;
 }
-
+// prosedur tampil hasil fibonacci
 void tampilHasilFibonacci(){
     if (cekFibonacci())
         cout << angka << " adalah bilangan fibonacci." << endl;
     else
         cout << angka << " bukan bilangan fibonacci." << endl;
 }
-
+// function tampil menu
 void tampilMenu() {
     cout << "\n=== MENU ===" << endl;
     cout << "1. Cek Bilangan Prima" << endl;
